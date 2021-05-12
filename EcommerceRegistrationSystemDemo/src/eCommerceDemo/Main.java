072e81356eadf6eff776bf2b8625dee36f9ce891
Furkan Cengiz <76488131+FurkanCengiz@users.noreply.github.com>
2021-05-12 16:29:04 +0300

    Update Main.java

--- EcommerceRegistrationSystemDemo/src/eCommerceDemo/Main.java
+++ EcommerceRegistrationSystemDemo/src/eCommerceDemo/Main.java
@@ -11,17 +11,6 @@ import eCommerceDemo.entities.concretes.User;
 public class Main {
 
 	public static void main(String[] args) {
-
-		/*
-		 * System.out.
-		 * println(" -------------------- E ticaret giriþ --------------------");
-		 * System.out.println("1-Kayýt ol"); System.out.println("1-giriþ yap"); Scanner
-		 * scan= new Scanner(System.in); int choose; choose = scan.nextInt();
-		 * switch(choose) { case 1: System.out.println("---- Kayýt ----"); User user =
-		 * new User(1,"Furkan","Cengiz","furkancengiz7@hotmail.com","123456");
-		 * UserService userService = new UserManager(new HibernateUserDao(),new
-		 * EmailValidator() ); userService.register(user); }
-		 */
 		
 		User user = new User(1, "furkan", "cengiz ", "furkancengiz@hotmail.com", "123456");
 		
